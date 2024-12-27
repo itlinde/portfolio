0a5f73b101a32307e7fcfe22b9c0a9a6f43e6fa9
bobabeeboo <isabellalinde1770@gmail.com>
2024-12-27 01:01:45 -0800

    v2 update

---
+++ src/app/about/page.js
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+export default function Page() {
+    return (
+    <div className="flex flex-wrap-reverse max-w-screen-xl mx-auto w-full items-center p-7 justify-center md:space-x-14">
+        <div className="max-w-md p-4">
+            <h2>about me</h2>
+            <p>Hi! I'm Isabella, a front-end enthusiast and enjoyer of really good design.</p>
+            <br/>
+            <p>I love front-end development because it combines my passion for design and software development into one. My goal is to one day work at one of the world's leading design firms, where I'll be able to contribute to products that impact millions worldwide.</p>
+            <br/>
+            <p>Recently, I've enjoyed working with: </p>
+            <ul className="list-disc list-inside">
+                <li>React & Next.js</li>
+                <li>Tailwind CSS</li>
+                <li>HTML/CSS</li>
+                <li>JavaScript</li>
+                <li>Swift</li>
+            </ul>
+            <br/>
+            <p>When I'm not geeking out over my latest project, you'll find me at concerts and jazz cafés, taking walks with my camera, filmmaking with friends, and saving up for my next trip abroad. </p>
+        </div>
+        <div className="w-80 min-w-screen-sm h-96 relative p-4">
+            <Image src="/images/me2.png" fill={true} className="object-cover rounded-2xl" alt="Picture of Isabella"/>
+            
+        </div>
+    </div>
+    );
+}
---
+++ src/app/page.js
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center">
+      <div id="content" className="max-w-screen-lg flex flex-wrap p-7 w-screen h-[85dvh] items-center justify-evenly">
+        <div className="">
+          <h2 className="block font-averia text-3xl text-textSecondary justify-start">Hey, it's</h2>
+          <h1 className="block leading-none justify-start">Isabella :)</h1>
+        </div>
+
+        <div className="group">
+          <Image src="/images/smile3.png" width={200} height={200} className="group-hover:hidden" alt="face"></Image>
+          <Image src="/images/smile1.png" width={200} height={200} className="hidden group-hover:block" alt="face"></Image>
+        </div>
+      </div>
+    </div>
+  );
+}
