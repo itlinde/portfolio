1460fb8bca8863514c02d1f469baac6a068a74ff
bobabeeboo <isabellalinde1770@gmail.com>
2025-02-03 14:23:46 -0800

    change bio

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -19,7 +19,7 @@ export default function Page() {
                 <li>Swift</li>
             </ul>
             <br/>
-            <p>When I'm not geeking out over my latest project, you'll find me at concerts and jazz cafés, taking walks with my camera, making short films with friends, and saving up for my next trip abroad. ✈️</p>
+            <p>When I'm not knee-deep in VSCode, you can find me at concerts and jazz cafés, taking walks with my camera, making short films with friends, and saving up for my next trip abroad. ✈️</p>
         </div>
         <div className="w-80 min-w-screen-sm h-96 relative p-4">
             <Image src="/images/me2.png" fill={true} className="object-cover rounded-2xl shadow-md shadow-secondary-500/70 border-2 border-secondary-500 transition ease-in-out duration-500 hover:scale-105 hover:-skew-y-2 hover:shadow-xl hover:shadow-secondary-500/30" alt="Picture of Isabella"/>
